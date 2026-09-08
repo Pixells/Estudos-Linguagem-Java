cc71316e05ad7c319ba4b7c0a5141372b5753081
Erik Matheus <80124753+Pixells@users.noreply.github.com>
2026-09-08 02:15:59 -0300

    Processamento e Casting em Java

---
+++ processamento-e-casting-java/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Locale;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		
+//		int a, b; 
+//		double resultado;
+//		a = 5; 
+//		b = 2; 
+//		resultado = (double) a / b; 
+//		System.out.println(resultado); 
+		
+		double a; 
+		int b; 
+		a = 5.0; 
+		b = (int) a; 
+		System.out.println(b); 
+	}	
+}
