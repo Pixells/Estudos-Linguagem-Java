3d64ddda6cbf1bec407e5027259639bb9dfd5ef9
Erik Matheus <80124753+Pixells@users.noreply.github.com>
2026-09-08 02:17:41 -0300

    Estrutura de Controle - Java

---
+++ estrutura-de-controle-java/src/Main.java
@@ -0,0 +1,58 @@
+	
+//		ISSO É SOMENTE UM MODELO A SER SEGUIDO PARA TESTES DE ESTRUTURA DE SEQUENCIA, CONDICAO E REPETITIVAS EM JAVA.
+		
+//		SIMPLES!!!
+//		if (condição) { 
+//		    comando1 
+//		    comando2 
+//		} 
+//		
+//		_________________________________
+//		
+//		COMPOSTA!!!
+//		if (condição) { 
+//		    comando1 
+//		    comando2 
+//		} 
+//		else { 
+//		    comando3 
+//		    comando4 
+//		} 
+//		
+//		_________________________________
+//		
+//		ENCADEAMENTO!!!
+//		if (condição1) { 
+//		    comando1 
+//		    comando2 
+//		} 
+//		else if (condição2) { 
+//		    comando3 
+//		    comando4 
+//		} 
+//		else { 
+//		    comando5 
+//		    comando6 
+//		} 
+//		
+//		_________________________________
+//		
+//		ESTRUTURA ENQUANTO EM Java 
+//		while (condição) { 
+//		    comando1 
+//		    comando2 
+//		} 
+//		
+//		ESTRUTURA PARA EM Java 
+//		for (inicio; condicao; incremento) {
+//			comando1
+//			comaando2
+//		}
+//		
+//		ESTRUTURA DO-WHILE EM Java (variante da estrutura REPITA-ATÉ) 
+//		do {
+//			comando1
+//			comando2
+//		} while (condicao);
+//		
+//	}
