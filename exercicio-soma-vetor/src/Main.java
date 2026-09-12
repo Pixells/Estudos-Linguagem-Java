2e59cf829862d0a9a4d4c4e7ad1ce9d3ea2f525b
Erik Matheus <80124753+Pixells@users.noreply.github.com>
2026-09-12 10:47:00 -0300

    exercicio-soma-vetor

---
+++ exercicio-soma-vetor/src/Main.java
@@ -0,0 +1,44 @@
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		
+		int N, i;
+		double soma, media;
+		
+		System.out.print("Quantos numeros voce vai digitar? ");
+		N = sc.nextInt();
+		
+		double[] vet = new double[N];
+		
+		for (i = 0; i < N; i++) {
+			System.out.print("Digite um numero: ");
+			vet[i] = sc.nextDouble();
+		}
+		
+		System.out.println();
+		System.out.print("VALORES = ");
+		for (i = 0; i < N; i++) {
+			System.out.print(String.format("%.1f", vet[i]) + " ");
+		}
+		
+		soma = 0;
+		System.out.println();
+		for (i = 0; i < N; i++) {
+			soma = (double) soma + vet[i];
+		}
+		System.out.println("SOMA = " + String.format("%.2f", soma));
+		
+		media = 0;
+		media = soma / N;
+		System.out.println("MEDIA = " + String.format("%.2f", media));
+		
+		
+		sc.close();
+	}
+}
